b6df49f8619707d025f5c7c47921961fe8dbb0d3
opilane <opilane@HP-CZC6297BB1>
2020-10-23 12:24:46 +0300

    Add project files.

---
+++ plan/plan/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace plan
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			StackLayout st = new StackLayout();
+			Button mon_btn = new Button { Text = "Monday" };
+			Button tue_btn = new Button { Text = "Tuesday" };
+			Button wed_btn = new Button { Text = "Wednesday" };
+			Button thu_btn = new Button { Text = "Thursday" };
+			Button fri_btn = new Button { Text = "Friday" };
+			Button sat_btn = new Button { Text = "Saturday" };
+			Button sun_btn = new Button { Text = "Sunday" };
+
+			st.Children.Add(mon_btn);
+			st.Children.Add(tue_btn);
+			st.Children.Add(wed_btn);
+			st.Children.Add(thu_btn);
+			st.Children.Add(fri_btn);
+			st.Children.Add(sat_btn);
+			st.Children.Add(sun_btn);
+
+			mon_btn.Clicked += new EventHandler(Buttons_clicked);
+			tue_btn.Clicked += new EventHandler(Buttons_clicked);
+			wed_btn.Clicked += new EventHandler(Buttons_clicked);
+			thu_btn.Clicked += new EventHandler(Buttons_clicked);
+			fri_btn.Clicked += new EventHandler(Buttons_clicked);
+			sat_btn.Clicked += new EventHandler(Buttons_clicked);
+			sun_btn.Clicked += new EventHandler(Buttons_clicked);
+
+			Content = st;
+		}
+
+		private async void Buttons_clicked(object sender, EventArgs e)
+		{
+			var btn = (Button)sender;
+			switch (btn.Text)//Проверяем текст на кнопке.
+			{
+				case "Monday":
+					await Navigation.PopAsync();//Закрываем страницу на которой находились.
+					await Navigation.PushAsync(new Monday());//Открываем нужную страницу.
+					break;
+
+				case "Tuesday":
+					await Navigation.PopAsync();//Закрываем страницу на которой находились.
+					await Navigation.PushAsync(new Tuesday());//Открываем нужную страницу.
+					break;
+
+				case "Wednesday":
+					await Navigation.PopAsync();//Закрываем страницу на которой находились.
+					await Navigation.PushAsync(new Wednesday());//Открываем нужную страницу.
+					break;
+
+				case "Thursday":
+					await Navigation.PopAsync();//Закрываем страницу на которой находились.
+					await Navigation.PushAsync(new Thursday());//Открываем нужную страницу.
+					break;
+
+				case "Friday":
+					await Navigation.PopAsync();//Закрываем страницу на которой находились.
+					await Navigation.PushAsync(new Friday());//Открываем нужную страницу.
+					break;
+
+				case "Saturday":
+					await Navigation.PopAsync();//Закрываем страницу на которой находились.
+					await Navigation.PushAsync(new Saturday());//Открываем нужную страницу.
+					break;
+
+				case "Sunday":
+					await Navigation.PopAsync();//Закрываем страницу на которой находились.
+					await Navigation.PushAsync(new Sunday());//Открываем нужную страницу.
+					break;
+			}
+		}
+	}
+}
---
+++ plan/plan/Monday.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace plan
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Monday : ContentPage
+	{
+		public Monday()
+		{
+			string[] tasks = new string[] { "Просыпаемся", "Делаем зарядку", "Завтракаем: КАШКА манная, ЧАЁК, орешки", "Соборимаемся", "Приезжаем в аэропорт", "Прилетаем в Париж", "ДОООООооооООООооооОООооООоОЛГО ГУЛЯЕМ с Наташей", "Обедаем с Наташей: ОБЕД ЧЕМПИОНОВ", "Едем к Наташе", "Чиллим под пивас, И КВАС feat. Whiskey", "Ложимся спать" };
+			ListView list = new ListView();
+			list.ItemsSource = tasks;
+			list.ItemSelected += List_ItemSelected;
+			Content = new StackLayout { Children = { list } };
+
+		}
+		string time;
+		private async void List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			if (e.SelectedItem != null)
+			{
+				string text = e.SelectedItem.ToString();
+				if (e.SelectedItemIndex == 0)//просыпаемся
+				{
+					time = "6:30";
+				}
+
+				else if (e.SelectedItemIndex == 1)//зарядка
+				{
+					time = "6:45";
+				}
+
+				else if (e.SelectedItemIndex == 2)//завтракаем
+				{
+					time = "7:05";
+				}
+
+				else if (e.SelectedItemIndex==3)//собираемся
+				{
+					time = "7:35";
+				}
+
+				else if (e.SelectedItemIndex==4)//приезжаем в аэропорт
+				{
+					time = "8:05";
+				}
+
+				else if (e.SelectedItemIndex==5)// прилетаем в Париж
+				{
+					time = "13:00";
+				}
+
+				else if (e.SelectedItemIndex==6)//гуляем с Наташей
+				{
+					time = "13:25";
+				}
+
+				else if (e.SelectedItemIndex==7)//обедаем с Наташей
+				{
+					time = "16:00";
+				}
+
+				else if (e.SelectedItemIndex==8)//едем к Наташе
+				{
+					time = "18:45 - по пути нас занесло в Хез";
+				}
+
+				else if (e.SelectedItemIndex==9)//отдыхаем с Наташей
+				{
+					time = "20:55";
+				}
+
+				else if (e.SelectedItemIndex==10)//СПАТЬ
+				{
+					time = "А мы точно легли????????";
+				}
+
+				await DisplayAlert(time, text, "Да");
+			}
+
+		}
+	}
+}
